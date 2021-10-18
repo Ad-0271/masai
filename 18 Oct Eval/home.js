681283c31427b84704242d8bb3d0d46dca4d1dc6
Ad-0271 <adnansari2001@gmail.com>
2021-10-18 14:50:24 +0530

    after 20min

--- 18 Oct Eval/home.js
+++ 18 Oct Eval/home.js
@@ -2,7 +2,7 @@
 
 async function getTopStories(){
 
-    let res = await fetch(`https://newsapi.org/v2/top-headlines?apiKey=4ec475e9bfdf4c0b8baf308ee9a88afe`)
+    let res = await fetch(`http://newsapi.org/v2/top-headlines?country=us&apiKey=4ec475e9bfdf4c0b8baf308ee9a88afe`)
 
     let data = await res.json();
 
