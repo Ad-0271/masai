b977fabf4b55b5e90739cc524b82e8e31d4aed56
Ad-0271 <adnansari2001@gmail.com>
2021-10-18 15:12:25 +0530

    after 20min

--- 18 Oct Eval/home.js
+++ 18 Oct Eval/home.js
@@ -6,7 +6,33 @@ async function getTopStories(){
 
     let data = await res.json();
 
-    console.log(data);
+    appendNews(data.articles);
 }
 
 getTopStories();
+
+let container = document.getElementById('topstories');
+
+function appendNews(news_data){
+
+    news_data.forEach((data) => {
+        let div = document.createElement('div');
+
+        let imgdiv = document.createElement('div');
+        
+        let img = document.createElement('img');
+        img.src = data.urlToImage;
+        img.style.width = '100%';
+        console.log(data.urlToImage)
+
+
+        imgdiv.append(img);
+        div.append(imgdiv);
+        container.appendChild(div);
+    })
+
+    
+
+
+
+}
