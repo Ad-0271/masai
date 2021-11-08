73755b668f6a20bfe5bacd7c241539c714a0cf17
Ad-0271 <adnansari2001@gmail.com>
2021-11-08 14:52:46 +0530

    after 20 min

---
+++ 08 Nov Eval/components/navbar.js
@@ -0,0 +1,11 @@
+function navbar(){
+
+    return `
+                <div id="options">
+                    <h3><a href="menu.html">Menu</a></h3>
+                    <h3><a href="login.html">Login</a></h3>
+                    <h3><a href="signup.html">Signup</a></h3>
+                </div>`
+}
+
+export default navbar
