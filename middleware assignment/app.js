cd0f25b7b1b4ee0c137b8d54b3d92af4300add72
Ad-0271 <adnansari2001@gmail.com>
2021-11-20 22:20:04 +0530

    middleware assignment

---
+++ middleware assignment/app.js
@@ -0,0 +1,54 @@
+let books = require('./books.json');
+
+const express = require('express');
+
+const app = express();
+
+const logger = (req, res, next) => {
+    req.name = "Adnan";
+    next();
+}
+
+app.use(logger);
+
+app.get('/', (req, res) => {
+    res.send({api_requested_by: req.name, books});
+})
+
+app.get('/books/:id', (req, res) => {
+    const book = books.filter((el) => {
+        return el.id == req.params.id;
+    });
+    res.json({api_requested_by : req.name, book});
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body);
+    res.send({api_requested_by : req.name, books});
+})
+
+app.patch('/books/:id', (req, res) => {
+    books = books.map((el) => {
+        if (el.id == req.params.id){
+            if (req?.body?.author) el.author = req.body.author;
+            if (req?.body?.book_name) el.book_name = req.body.book_name;
+            if (req?.body?.pages) el.pages = req.body.pages;
+            if (req?.body?.published_year) el.published_year = req.body.published_year;
+        }
+        return el;
+    });
+
+    res.send({api_requested_by: req.name, books});
+})
+
+app.delete('/books/:id', (req, res) => {
+    books = books.filter((el) => {
+        return el.id != req.params.id;
+    })
+
+    res.send({api_requested_by: req.name, books})
+})
+
+app.listen(2701, () => {
+    console.log('server is running...')
+});
