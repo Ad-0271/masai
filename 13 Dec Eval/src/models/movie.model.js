97f88e870c5387178788ab1de2d6f6ee9477ed6c
Ad-0271 <adnansari2001@gmail.com>
2021-12-13 14:45:24 +0530

    after 20 min

---
+++ 13 Dec Eval/src/models/movie.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    actors: [{type: String, required: true}],
+    languages: [{type: String, required: true}],
+    directors: [{type: String, required: true}]
+})
+
+module.exports = mongoose.model('movie', movieSchema);
