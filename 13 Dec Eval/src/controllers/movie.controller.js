5263bf7891c87f9dff58364a9136459b7b0c0380
Ad-0271 <adnansari2001@gmail.com>
2021-12-13 17:35:23 +0530

    final commit

--- 13 Dec Eval/src/controllers/movie.controller.js
+++ 13 Dec Eval/src/controllers/movie.controller.js
@@ -10,5 +10,16 @@ router.get('/:id', crudController.getOne(Movie));
 router.post('', crudController.post(Movie));
 router.patch('/:id', crudController.updateOne(Movie));
 router.delete('/:id', crudController.deleteOne(Movie));
+//get movie of a actor
+router.get('/actor/:id', async (req, res) => {
+    try{
+        console.log(req.params.id)
+        const items = await Movie.find({actors: {$eq: req.params.id}}).lean().exec();
+        
+        res.status(200).send(items);
+    } catch(e) {
+        res.status(500).send({status: 'fail', message: e.message});
+    }
+})
 
 module.exports = router;
